3591d8d8c92ce4eb1ba27c32d549b591e65ff716
Camila Medeiros <89259285+camilamedeir0s@users.noreply.github.com>
2025-03-06 11:19:10 -0300

    Add files via upload

---
+++ shared-iterations-test.js
@@ -0,0 +1,46 @@
+import http from 'k6/http';
+import { check } from 'k6';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+
+// Obtendo valores de variáveis de ambiente ou usando padrões
+const VUS = parseInt(__ENV.VUS) || 500; // Número de usuários virtuais simultâneos
+const OUTPUT = __ENV.OUTPUT || 'raw-data.json'; // Nome do arquivo de saída
+const HOST = "localhost"
+
+export const options = {
+  scenarios: {
+    fixed_iterations: {
+      executor: "shared-iterations",
+      vus: VUS,
+      iterations: VUS * 500,
+      maxDuration: "5m",
+    },
+  },
+};
+
+const endpoints = [
+  '/',
+  '/productpage',
+  '/api/v1/products',
+  '/api/v1/products/1',
+  '/api/v1/products/1/reviews',
+  '/api/v1/products/1/ratings',
+];
+
+function checkResponse(res) {
+  check(res, { 'status was 200': (r) => r.status === 200 });
+}
+
+export default function () {
+  for (const endpoint of endpoints) {
+    const res = http.get(`http://${HOST}${endpoint}`);
+    checkResponse(res);
+  }
+}
+
+export function handleSummary(data) {
+  return {
+    [OUTPUT]: JSON.stringify(data), // Usa o nome do arquivo especificado em OUTPUT
+    stdout: textSummary(data, { indent: ' ', enableColors: true }),
+  };
+}
