a95d31a49efddbce8f6d209fd2a862f0b36d31a3
Camila Medeiros <89259285+camilamedeir0s@users.noreply.github.com>
2025-03-11 20:10:01 -0300

    Update shared-iterations-test.js

--- shared-iterations-test.js
+++ shared-iterations-test.js
@@ -1,11 +1,23 @@
 import http from 'k6/http';
 import { check } from 'k6';
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+import {
+  AWSConfig,
+  S3Client,
+} from 'https://jslib.k6.io/aws/0.12.3/s3.js';
+
+const s3 = new S3Client(new AWSConfig({
+  region: "us-east-1",
+  accessKeyId: __ENV.AWS_ACCESS_KEY_ID,
+  secretAccessKey: __ENV.AWS_SECRET_ACCESS_KEY,
+}));
 
 // Obtendo valores de variáveis de ambiente ou usando padrões
 const VUS = parseInt(__ENV.VUS) || 500; // Número de usuários virtuais simultâneos
 const OUTPUT = __ENV.OUTPUT || 'raw-data.json'; // Nome do arquivo de saída
 const HOST = __ENV.HOST || "localhost";
+
+const bucketName = __ENV.BUCKET_NAME;
 
 export const options = {
   scenarios: {
@@ -39,6 +51,13 @@ export default function () {
 }
 
 export function handleSummary(data) {
+  try {
+    const response = s3.putObject(bucketName, OUTPUT, JSON.stringify(data, null, 2));
+    console.log(`Upload para S3 bem-sucedido! Resposta: ${JSON.stringify(response)}`);
+  } catch (error) {
+    console.error(`Erro ao enviar para S3: ${error.message}`);
+  }
+
   return {
     [OUTPUT]: JSON.stringify(data), // Usa o nome do arquivo especificado em OUTPUT
     stdout: textSummary(data, { indent: ' ', enableColors: true }),
